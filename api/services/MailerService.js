9d76578ee48f42a4a9cdc726ef919c7a0e8b55d4
Mahendra Hiraman Bagul <mahendrabagul@users.noreply.github.com>
2015-04-19 15:47:57 +0530

    Update MailerService.js

--- api/services/MailerService.js
+++ api/services/MailerService.js
@@ -14,8 +14,8 @@ module.exports = {
 		// setup e-mail data with unicode symbols
 		var mailOptions = {
 		    from: 'Task Manager App ✔ <mahisapbagul@gmail.com>', // sender address
-		    // to: user.email, // list of receivers
-		    to: "bagulm123@gmail.com", 
+		    to: user.email, // list of receivers
+		    //to: "bagulm123@gmail.com", 
 		    subject: 'Verify and Confirm your mail Id ✔', // Subject line
 		    html: 'Please click on this link to activate your account '
 		    +sails.getBaseurl()+"/confirmaccount?email="+user.email+"&token="+user.token // html body
@@ -44,8 +44,8 @@ module.exports = {
 		// setup e-mail data with unicode symbols
 		var mailOptions = {
 		    from: 'Task Manager App ✔ <mahisapbagul@gmail.com>', // sender address
-		    // to: user.email, // list of receivers
-		    to: "bagulm123@gmail.com", 
+		    to: user.email, // list of receivers
+		    //to: "bagulm123@gmail.com", 
 		    subject: 'Reset your password✔', // Subject line
 		    html: 'Please click on this link to reset your password '
 		    +sails.getBaseurl()+"/createnewpassword?email="+user.email+"&token="+user.token // html body
@@ -74,8 +74,8 @@ module.exports = {
 		// setup e-mail data with unicode symbols
 		var mailOptions = {
 		    from: 'Task Manager App ✔ <mahisapbagul@gmail.com>', // sender address
-		    // to: user.email, // list of receivers
-		    to: "bagulm123@gmail.com", 
+		    to: user.email, // list of receivers
+		    //to: "bagulm123@gmail.com", 
 		    subject: 'Your Password has been changed successfully ✔', // Subject line
 		    html: 'Thanks Task Manager App Team' // html body
 		};
